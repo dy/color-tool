8ad425e955b2b0ec0f0a29004d8993afe68429c0
Dmitry <df.creative@gmail.com>
2015-06-03 14:10:08 +0300

    Make first rendering of color

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ var throttle = require('emmy/throttle');
 var getUid = require('get-uid');
 var spaces = require('color-space');
 var Slidy = require('slidy');
-var isArray = require('is-array');
-var isNumber = require('is-number');
+var isArray = require('mutype/is-array');
+var isNumber = require('mutype/is-number');
 
 
-module.exports = Picker;
+module.exports = Picky;
 
 
 
@@ -37,7 +37,7 @@ var isWorkerAvailable = typeof Worker !== 'undefined';
  * @link see references in color-ranger tests
  */
 if (isWorkerAvailable) {
-	worker = getWorker(spaces);
+	var WORKER = getWorker(spaces);
 }
 
 
@@ -47,49 +47,94 @@ if (isWorkerAvailable) {
  * This is a main picker class.
  * It provides an abstract interface for any color picker.
  */
-function Picker (element, options) {
+function Picky (target, options) {
 	var self = this;
 
-	this.element = element;
+	//force constructor
+	if (!(self instanceof Picky)) return new Picky(target, options);
 
-	//parse attributes of a targret
-	// var prop, parseResult;
-	// for (var propName in constr.options){
-	// 	//parse attribute, if no option passed
-	// 	if (options[propName] === undefined){
-	// 		prop = constr.options[propName];
-	// 		options[propName] = parse.attribute(element, propName, prop && prop.init !== undefined ? prop.init : prop);
-	// 	}
+	//ensure target & options
+	if (!options) {
+		if (target instanceof Element) {
+			options = {};
+		}
+		else {
+			options = target;
+			target = doc.createElement('div');
+		}
+	}
 
-	// 	//declare initial value
-	// 	if (options[propName] !== undefined) {
-	// 		this[propName] = options[propName];
-	// 	}
-	// }
+
+	//get preferred element
+	self.element = target;
 
 	//add class
-	this.element.classList.add('picky');
+	self.element.classList.add('picky');
 
 	//take over options
-	extend(this, options);
-
+	extend(self, options);
 
 	//generate uid for worker
-	this.id = getUid();
+	self.id = getUid();
 
-	//create color instance
-	this.color = new Color(this.color);
+	//save channel indexes
+	var space = spaces[self.space];
 
-	//make self a slidy
-	this.slidy = new Slidy(element, {
-		pickerClass: 'picky-picker'
+	self._channels = (isArray(self.channel) ? self.channel : [self.channel] ).map(function (channel) {
+		if (isNumber(channel)) return channel;
+		var idx = space.channel.indexOf(channel);
+		if (idx < 0) throw Error('Space ' + self.space + ' has no channel ' + channel);
+		return idx;
 	});
 
+	//save channel mins/maxes
+	self._min = self._channels.map(function (idx) {
+		return space.min[idx];
+	});
+	self._max = self._channels.map(function (idx) {
+		return space.max[idx];
+	});
 
-	//Events
+	//create color instance
+	self.color = new Color(self.color);
+	var values = self.color[self.space + 'Array']();
+
+	//make self a slidy
+	self.slidy = new Slidy(target, {
+		pickerClass: 'picky-picker',
+		point: true,
+		min: self._min,
+		max: self._max,
+
+		//set initial value as the color
+		value: self._channels.map(function (idx) {
+			return values[idx];
+		})
+	});
+
+	//enable events
+	self.enable();
+
+	//force update
+	self.update();
+}
+
+
+var proto = Picky.prototype = Object.create(Emitter.prototype);
+
+
+/** Enable interactions */
+proto.enable = function () {
+	var self = this;
+
+	//emit self change
+	self.slidy.on('change', function (value) {
+		self.emit('change', self.color);
+	});
+
 	//listen to bg update events for the specifically this picker
-	if (isWorkerAvailable && this.worker) {
-		on(worker, 'message', function(e){
+	if (isWorkerAvailable && self.worker) {
+		on(WORKER, 'message', function (e) {
 			if (e.data.id === self.id) self.renderData(e.data.data);
 		});
 	}
@@ -100,25 +145,14 @@ function Picker (element, options) {
 	//rerender on color change - loosely calling
 	//50 is the most appropriate interval for bg
 	//10 is the interval for picker movement
-	// throttle(this.color, 'change', 50, function(e){
-	// 	self.colorChanged.call(self, e);
-	// });
-
-	//change color on self slidy change
-	on(this.slidy, 'change', function (e) {
-		self.emit('change');
+	throttle(self.color, 'change', 50, function (e) {
+		self.update();
 	});
 
-
-	//force update
-	self.update();
-}
+};
 
 
-var proto = Picker.prototype = Object.create(Emitter.prototype);
-
-
-/** Basic color for pickers */
+/** Basic initial color for pickers */
 proto.color = new Color();
 
 
@@ -142,38 +176,19 @@ proto.update = function () {
 	var self = this;
 
 	var imgData = ctx.getImageData(0, 0, cnv.width, cnv.height);
-	var space = spaces[this.space];
-
-	//form channels
-	var channels = self.channel;
-	if (!isArray(self.channel)) {
-		channels = [self.channel];
-	}
-	channels = channels.map(function (channel) {
-		if (isNumber(channel)) return channel;
-		var idx = space.channel.indexOf(channel);
-		if (idx < 0) throw Error('Space ' + self.space + ' has no channel ' + channel);
-		return idx;
-	});
-
-	//form mins, maxes
-	var mins = [], maxes = [];
-	channels.forEach(function (chNum, i) {
-		mins[i] = space.min[chNum];
-		maxes[i] = space.max[chNum];
-	});
+	var space = spaces[self.space];
 
 	//form options
 	var opts = {
 		space: this.space,
-		channel: channels,
-		max: mins,
-		min: maxes,
+		channel: self._channels,
+		max: self._max,
+		min: self._min,
 	};
 
 	//render range for a new color value in worker
 	if (isWorkerAvailable && this.worker && false) {
-		worker.postMessage(extend(opts, {
+		WORKER.postMessage(extend(opts, {
 			rgb: this.color.rgbArray(),
 			data: imgData.data,
 			id: this.id
