7046eee917d62037658115cd763f5d8b309c20fb
Dmitry <df.creative@gmail.com>
2015-06-07 23:15:00 +0300

    Init picky pre-alpha page

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
 var Picker = require('color-picky');
 var q = require('queried');
+var Color = require('color');
+
+
+var doc = document, loc = doc.location, win = window;
+
+
+/** Get initial color as url param */
+var cstr = loc.href.slice(loc.origin.length + 1);
+var color = Color(cstr || 'red');
+
 
 /** Init first big demo picker */
-
 var demoPickerEl = q('.section-demo-picker');
 
-new Picker(demoPickerEl, {
-	space: 'lab',
-	channel: ['lightness', 'a']
+var demoPicker = new Picker(demoPickerEl, {
+	space: 'hsl',
+	channel: ['hue', 'lightness'],
+	worker: false,
+	color: color,
+	change: function () {
+		//update picker color
+		this.element.style.color = this.color.rgbString();
+		this.element.firstChild.style.backgroundColor = this.color.rgbString();
+
+		//update document location
+		var cstr = this.color.hexString()//.replace(/\s/g, '');
+		win.history.replaceState(null, cstr, cstr);
+	}
+});
+
+var demoPickerEl = q('.section-demo-picker-s');
+
+var demoPicker = new Picker(demoPickerEl, {
+	space: 'hsl',
+	channel: ['saturation'],
+	worker: false,
+	color: color,
+	change: function () {
+		//update picker color
+		this.element.style.color = this.color.rgbString();
+		this.element.firstChild.style.backgroundColor = this.color.rgbString();
+
+		//update document location
+		var cstr = this.color.hexString()//.replace(/\s/g, '');
+		win.history.replaceState(null, cstr, cstr);
+	}
 });
